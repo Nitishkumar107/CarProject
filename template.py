be459e86df74f948b6a9489636c008e08551b743
Nitish kumar <nitish10795@gmail.com>
2024-04-29 17:54:26 +0530

    project all file created in src file

--- template.py
+++ template.py
@@ -23,30 +23,32 @@ list_of_files = [
     f"src/{project_name}/utils.py",
     "app.py"
     "Dockerfile",
-    # "README.md",
+    "README.md",
     "setup.py",
-    #"tests/__init__.py",
-    #"tests/conftest.py",
-    #"tests/test_data_ingestion.py",
+    "tests/__init__.py",
+    "tests/conftest.py",
+    "tests/test_data_ingestion.py",
     "requirements.txt"
 ]
+
 for filepath in list_of_files:
     filepath = Path(filepath)
-    filedir , filename = os.path.split(filepath)
+    filedir, filename = os.path.split(filepath)
 
-    if filedir != " ":
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Created directory: {filedir} for the file: {filename}")
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
 
+    
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath,"w") as f :
+        with open(filepath,'w') as f:
             pass
-            logging.info(f"Created empty file: {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
 
+
+    
     else:
-        logging.info(f"File: {filepath} {filename} already exists")
-
-
+        logging.info(f"{filename} is already exists")
 
 
 
